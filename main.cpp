ed050053854f396f5148626d75cce07042d4e48d
marko <marko.pavicevic@outlook.com>
2026-01-12 12:19:29 -0800

    Implemented render_left() and render_right() helper functions. Added randomization to tests.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
+#include <chrono>
 #include <fstream>
 #include <ncurses.h>
+#include <random>
 #include <string>
 
 using namespace std;
+
+// Renders the characters to the left of the cursor.
+void render_left(const string& typed, const string& words, int cursor_x, int cursor_y) {
+  for (size_t i = 0; i < typed.size(); i++) {
+    int x_pos = cursor_x - (typed.size() - i);
+    if (typed[i] == words[i]) attron(COLOR_PAIR(1));
+    else attron(COLOR_PAIR(2));
+    mvaddch(cursor_y, x_pos, typed[i]);
+    attroff(COLOR_PAIR(1));
+    attroff(COLOR_PAIR(2));
+  }
+}
+
+// Renders the characters to the right of the cursor.
+void render_right(const string& typed, const string& words, int cursor_x, int cursor_y) {
+  size_t pos = typed.size();
+  for (size_t i = 0; i < words.size(); i++) {
+    int x_pos = cursor_x + i;
+    if (x_pos >= COLS) break;
+    if (i == 0) {
+      attron(COLOR_PAIR(3));
+      mvaddch(cursor_y, x_pos, words[pos + i]);
+      attroff(COLOR_PAIR(3));
+    } else mvaddch(cursor_y, x_pos, words[pos + i]);
+  }
+}
 
 int main() {
   initscr();
@@ -14,69 +42,51 @@ int main() {
   curs_set(0);
 
   // Color pairs.
-  init_pair(1, COLOR_GREEN, -1);
-  init_pair(2, COLOR_RED, -1);
-  init_pair(3, COLOR_BLACK, COLOR_WHITE);
+  init_pair(1, COLOR_GREEN, -1);          // Correctly typed characters.
+  init_pair(2, COLOR_RED, -1);            // Incorrectly typed characters.
+  init_pair(3, COLOR_BLACK, COLOR_WHITE); // Pseudo cursor.
 
   // Cursor position.
   int cursor_x = COLS / 4;
   int cursor_y = LINES / 2;
 
-  // What has been typed.
+  // Characters to the left of the cursor.
   string typed = "";
 
-  // Generate and display test on launch.
+  // Characters to the right of the cursor.
   string words = "";
+
+  // Create word bank.
+  vector<string> word_bank;
   ifstream input_file("words.txt");
-  string word;
-  for (int i = 0; i < 100; i++) {
-    getline(input_file, word);
-    words += word + ' ';
-  }
+  string line;
+  while (getline(input_file, line)) word_bank.push_back(line + ' ');
   input_file.close();
 
-  size_t pos = typed.size();
-  for (size_t i = 0; i < words.size(); i++) {
-    int x_pos = cursor_x + i;
-    if (x_pos >= COLS) break;
-    if (i == 0) {
-      attron(COLOR_PAIR(3));
-      mvaddch(cursor_y, x_pos, words[pos + i]);
-      attroff(COLOR_PAIR(3));
-    } else mvaddch(cursor_y, x_pos, words[pos + i]);
+  // Generate test.
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  mt19937_64 engine(seed);
+  uniform_int_distribution<int> distribution(0, word_bank.size() - 1);
+  for (int i = 0; i < 100; i++) {
+    int random_index = distribution(engine);
+    words += word_bank[random_index];
   }
+
+  // Display test on launch.
+  render_right(typed, words, cursor_x, cursor_y);
 
   while (true) {
     // Read input from keyboard.
-    int ch = getch();
-    if (ch == 27) break;
-    if (('a' <= (char)ch && (char)ch <= 'z') || (char)ch == ' ') {
+    int in = getch();
+    if (in == 27) break; // ESC to exit.
+    if (('a' <= (char)in && (char)in <= 'z') || (char)in == ' ') {
       // Append input to string of typed characters.
-      typed += ch;
+      typed += in;
 
       // Render the test.
       clear();
-
-      for (size_t i = 0; i < typed.size(); i++) {
-        int x_pos = cursor_x - (typed.size() - i);
-        if (typed[i] == words[i]) attron(COLOR_PAIR(1));
-        else attron(COLOR_PAIR(2));
-        mvaddch(cursor_y, x_pos, typed[i]);
-        attroff(COLOR_PAIR(1));
-        attroff(COLOR_PAIR(2));
-      }
-
-      size_t pos = typed.size();
-      for (size_t i = 0; i < words.size(); i++) {
-        int x_pos = cursor_x + i;
-        if (x_pos >= COLS) break;
-        if (i == 0) {
-          attron(COLOR_PAIR(3));
-          mvaddch(cursor_y, x_pos, words[pos + i]);
-          attroff(COLOR_PAIR(3));
-        } else mvaddch(cursor_y, x_pos, words[pos + i]);
-      }
-
+      render_left(typed, words, cursor_x, cursor_y);
+      render_right(typed, words, cursor_x, cursor_y);
       refresh();
     }
   }
